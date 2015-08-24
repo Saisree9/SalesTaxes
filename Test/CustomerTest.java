0ebb30d409d414fd993612187689b570aa239c01
saisree <saisree@INsaisree.local>
2015-08-24 18:02:43 +0530

    "commit"

--- Test/CustomerTest.java
+++ Test/CustomerTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 /**
  * Created by saisree on 8/24/15.
  */
 public class CustomerTest {
+    @Test
+    public void testShouldTotalCostOfItemsIsCostOfItemIfIPurchaseBook(){
+        Customer customer =new Customer();
+        double totalAmount=customer.getreceipt();
+    }
+
 }
--- src/Customer.java
+++ src/Customer.java
@@ -2,4 +2,8 @@
  * Created by saisree on 8/24/15.
  */
 public class Customer {
+
+    public double getreceipt() {
+
+    }
 }
